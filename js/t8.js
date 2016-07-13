4a2cc74c131e498e4d54bafa992e87eb236a9a49
mumucxy <420199278@qq.com>
2016-07-13 11:59:12 +0800

    7-13上午

---
+++ js/t8.js
@@ -0,0 +1,31 @@
+$(function(){
+    $(".contain .left img").click(function(){
+    	var type = $(this).data("type");
+    	if(type == 0){
+    		$(this).css("transform","translate(150px,140px)");	
+        }else if(type == 1){
+        	$(this).css("transform","translate(150px,0px)");	
+        }
+        else if(type == 2){
+        	$(this).css("transform","translate(150px,-140px)");	
+        }
+        
+        var arr = $(".contain .right img");
+        var temp =$(arr[Math.floor(Math.random()*arr.length)]);
+        var type2 = temp.data("type")
+        if(type == 0){
+    		$(this).css("transform","translate(150px,140px)");	
+        }else if(type == 1){
+        	$(this).css("transform","translate(150px,0px)");	
+        }
+        else if(type == 2){
+        	$(this).css("transform","translate(150px,-140px)");	
+        }
+        var that =$(this);
+        setTimeout(function(){
+        	that.css("transform","translate(0px,0px)");
+        	temp.css("transform","translate(0px,0px)")
+        })
+        
+   });
+});
